d69c6c355e5931c08bde8a6ee60cda765ee24f71
Jesse Monroy <jessemonroy650@yahoo.com>
2016-02-25 19:03:59 -0700

    update

--- app.js
+++ app.js
@@ -2,6 +2,10 @@ var app = {
     onDeviceReady : function () {
         console.log('deviceready');
         document.getElementById('info').innerHTML = 'deviceready';
+        app.showIt('content', 'deviceready');
+    },
+    showIt : function (id, msg) {
+        document.getElementById(id).innerHTML = msg;
     }
 };
 
